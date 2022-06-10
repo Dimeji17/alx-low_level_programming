428a993f0c445f8f76c849e6607d17d68525fe24
Dimeji17 <oladimejialhassan17@gmail.com>
2022-06-10 01:41:02 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/** 
+*function to calculate the produt of two numbers
+* a is the first number
+* b is the second number
+**/
+int product(int a, int b)
+{
+    int p;
+    
+    p = a * b;
+return (0);
+}
