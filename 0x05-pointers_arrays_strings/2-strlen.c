d1371b9cd19311cbadc250070cdf385b25200b7b
Dimeji17 <oladimejialhassan17@gmail.com>
2022-06-13 14:13:52 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,3 +1,18 @@
+#include<stdio.h>
 #include "main.h"
 /**
- * 
+* _strlen - calculate and return length of string
+* @s : string to be evaluated
+* Return: length
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
