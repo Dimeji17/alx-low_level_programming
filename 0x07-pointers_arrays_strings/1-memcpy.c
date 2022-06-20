fdba50d8a4caa877bf6ac6acea6dec711b4ed628
Dimeji17 <oladimejialhassan17@gmail.com>
2022-06-20 14:08:18 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "mainn.h"
+/**
+ *_memcpy - a function that copies memory area
+ *@dest: memory where its stoted 
+ *@src: memory where is copied
+ *@n: number of bytes
+ *
+ *Return: copied memory with n byted changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (n > 0)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+		n--;
+	}
+	return (dest);
+}
