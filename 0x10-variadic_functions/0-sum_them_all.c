f10701573d0dc13e810b10d2b1c689e0a8d136ac
Dimeji17 <oladimejialhassan17@gmail.com>
2022-07-07 16:18:57 -0700

    .

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,16 +9,22 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int total = 0;
+	unsigned int total = 0;
 
 	va_list args;
 
 	va_start(args, n);
-	for (int i = 0; i < n; i++)
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+	for (unsigned int i = 0; i < n; i++)
 	{
 		total += va_arg(args, int);
 	}
 	va_end(args);
 	return (total);
 }
-
+}
