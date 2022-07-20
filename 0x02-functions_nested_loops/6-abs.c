a92f45c86fa947aeaae8cd54dab4d5d27df07886
Dimeji17 <66203025+Dimeji17@users.noreply.github.com>
2022-07-20 14:45:16 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,9 +6,19 @@
  * Return:0 or 1
  */
 
-int _abs(int n)
-{
+int _abs(int c)
+{   
+    
+    int negate;
 
-return (n >= 0 ? n : n * -1);
-
+    if (c >= 0)
+    {
+        return(c);
+    }
+    else if (c < 0)
+    {
+        negate = (c * (-1));
+        return(negate);
+    }
+    return(0);
 }
